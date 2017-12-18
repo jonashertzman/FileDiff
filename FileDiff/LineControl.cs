3dbc35ecfe3bc8572f125011caac7d4e619ac963
Jonas Hertzman <jonas.hertzman@gmail.com>
2017-12-18 23:02:00 +0100

    Correct size on lines

--- FileDiff/LineControl.cs
+++ FileDiff/LineControl.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FileDiff
 {
 	public class LineControl : Control
 	{
 
-		private Size textSize;
-
-		private Size size = new Size();
+		private Size characterSize = MeasureString("W");
+		protected FormattedText rowNumberText;
+		protected FormattedText lineText;
 
 		#region Constructors
 
@@ -33,8 +22,7 @@ namespace FileDiff
 
 		public LineControl()
 		{
-			textSize = MeasureString("W");
-			size = new Size(100, textSize.Height);
+
 		}
 
 		#endregion
@@ -43,56 +31,92 @@ namespace FileDiff
 
 		protected override Size MeasureOverride(Size constraint)
 		{
-			//Debug.Print("MeasureOverride");
-			return size;
+			//Debug.Print($"MeasureOverride rownumber:{RowNumber} text:{Text}");
+
+			if (lineText == null)
+			{
+				return characterSize;
+			}
+			return new Size(lineText.Width + 30, lineText.Height);
 		}
 
 		protected override void OnRender(DrawingContext drawingContext)
 		{
-			//Debug.Print("OnRender" + VisualOffset.ToString());
-			Typeface typeface = new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch);
-			FormattedText rowNumberText = new FormattedText(RowNumber.ToString(), CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, Brushes.Black, null, TextFormattingMode.Display);
-			drawingContext.DrawText(rowNumberText, new Point(0, 0));
+			//Debug.Print($"OnRender rownumber:{RowNumber} text:{Text}");
 
-			FormattedText lineText = new FormattedText(Text, CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, Brushes.Black, null, TextFormattingMode.Display);
-			drawingContext.DrawRectangle(new SolidColorBrush(AppSettings.Settings.DeletedBackground), new Pen(Brushes.Transparent, 0), new Rect(30, 0, lineText.Width, size.Height));
-			drawingContext.DrawText(lineText, new Point(30, 0));
+			var typeface = new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch);
+
+			if (RowNumber != -1)
+			{
+				rowNumberText = new FormattedText(RowNumber.ToString(), CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, Brushes.Black, null, TextFormattingMode.Display);
+				drawingContext.DrawText(rowNumberText, new Point(0, 0));
+			}
+
+			if (lineText != null)
+			{
+				drawingContext.DrawRectangle(new SolidColorBrush(AppSettings.Settings.DeletedBackground), new Pen(Brushes.Transparent, 0), new Rect(30, 0, lineText.Width, lineText.Height));
+				drawingContext.DrawText(lineText, new Point(30, 0));
+			}
 		}
 
 		#endregion
 
+		#region Properties
 
-		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(LineControl));
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(LineControl),
+			new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure, OnTextChanged));
+
 		public string Text
 		{
 			get { return (string)GetValue(TextProperty); }
 			set { SetValue(TextProperty, value); }
 		}
 
+		public static readonly DependencyProperty RowNumberProperty = DependencyProperty.Register("RowNumber", typeof(int), typeof(LineControl),
+			new FrameworkPropertyMetadata(-1, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure, OnRowNumberChanged));
 
-		public static readonly DependencyProperty RowNumberProperty = DependencyProperty.Register("RowNumber", typeof(int), typeof(LineControl));
 		public int RowNumber
 		{
 			get { return (int)GetValue(RowNumberProperty); }
 			set { SetValue(RowNumberProperty, value); }
 		}
 
+		#endregion
 
-		private Size MeasureString(string text)
+		#region Methods
+
+		private static void OnRowNumberChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+		}
+
+		private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((LineControl)d).SetText(e.NewValue as string);
+		}
+
+		private void SetText(string v)
+		{
+			var typeface = new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch);
+
+			lineText = new FormattedText(v, CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, Brushes.Black, null, TextFormattingMode.Display);
+		}
+
+		private static Size MeasureString(string text)
 		{
 			FormattedText formattedText = new FormattedText(
-					text,
-					CultureInfo.CurrentCulture,
-					FlowDirection.LeftToRight,
-					new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch),
-					this.FontSize,
-					Brushes.Black,
-					new NumberSubstitution(),
-					TextFormattingMode.Display);
+			text,
+			CultureInfo.CurrentCulture,
+			FlowDirection.LeftToRight,
+			new Typeface(AppSettings.Settings.Font),
+			AppSettings.Settings.FontSize,
+			Brushes.Black,
+			new NumberSubstitution(),
+			TextFormattingMode.Display);
 
 			return new Size(formattedText.Width, formattedText.Height);
 		}
 
+		#endregion
 
 	}
 }
