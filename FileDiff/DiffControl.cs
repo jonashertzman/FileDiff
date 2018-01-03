db8619801041818d70964c2f26be20f4ab4952da
Jonas Hertzman <jonas.hertzman@gmail.com>
2018-01-03 07:14:20 +0100

    .

--- FileDiff/DiffControl.cs
+++ FileDiff/DiffControl.cs
@@ -23,43 +23,50 @@ namespace FileDiff
 
 		#region Overrides
 
-		protected override Size MeasureOverride(Size constraint)
-		{
-			characterSize = MeasureString("W");
-			return new Size(1000, characterSize.Height * Lines.Count);
-		}
+		//protected override Size MeasureOverride(Size constraint)
+		//{
+		//	characterSize = MeasureString("W");
+		//	return new Size(1000, characterSize.Height * Lines.Count);
+		//}
 
 		protected override void OnRender(DrawingContext drawingContext)
 		{
+			if (Lines.Count == 0)
+				return;
+
 			characterSize = MeasureString("W");
 			int margin = (Lines.Count.ToString().Length * (int)characterSize.Width) + 3;
 
 			Typeface typeface = new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch);
 
-			int lineIndex = 0;
 
-			foreach (Line l in Lines)
+			for (int i = 0; i < VisibleLines; i++)
 			{
+				Line l = Lines[i + VerticalOffset];
+
 				if (l.LineIndex != -1)
 				{
 					if (l.Type != TextState.FullMatch)
 					{
-						drawingContext.DrawRectangle(new SolidColorBrush(AppSettings.Settings.DeletedBackground), new Pen(Brushes.Transparent, 0), new Rect(0, characterSize.Height * lineIndex, 1000, characterSize.Height));
+						drawingContext.DrawRectangle(new SolidColorBrush(AppSettings.Settings.DeletedBackground), new Pen(Brushes.Transparent, 0), new Rect(0, characterSize.Height * i, this.ActualWidth, characterSize.Height));
 					}
 					FormattedText rowNumberText = new FormattedText(l.LineIndex.ToString(), CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, SystemColors.ScrollBarBrush, null, TextFormattingMode.Display);
-					drawingContext.DrawText(rowNumberText, new Point(1, characterSize.Height * lineIndex));
+					drawingContext.DrawText(rowNumberText, new Point(1, characterSize.Height * i));
 
 					FormattedText lineText = new FormattedText(l.Text, CultureInfo.CurrentCulture, this.FlowDirection, typeface, this.FontSize, Brushes.Black, null, TextFormattingMode.Display);
-					drawingContext.DrawText(lineText, new Point(margin, characterSize.Height * lineIndex));
+					drawingContext.DrawText(lineText, new Point(margin, characterSize.Height * i));
 				}
-
-				lineIndex++;
 			}
 		}
 
 		#endregion
 
 		#region Properties
+
+		public int VisibleLines
+		{
+			get { return (int)(this.ActualHeight / characterSize.Height + 1); }
+		}
 
 		public static readonly DependencyProperty LinesProperty = DependencyProperty.Register("Lines", typeof(ObservableCollection<Line>), typeof(DiffControl),
 			new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
@@ -68,6 +75,25 @@ namespace FileDiff
 		{
 			get { return (ObservableCollection<Line>)GetValue(LinesProperty); }
 			set { SetValue(LinesProperty, value); }
+		}
+
+
+		public static readonly DependencyProperty VerticalOffsetProperty = DependencyProperty.Register("VerticalOffset", typeof(int), typeof(DiffControl),
+			new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsRender /*| FrameworkPropertyMetadataOptions.AffectsMeasure*/));
+
+		public int VerticalOffset
+		{
+			get { return (int)GetValue(VerticalOffsetProperty); }
+			set { SetValue(VerticalOffsetProperty, value); }
+		}
+
+		public static readonly DependencyProperty HorizontalOffsetProperty = DependencyProperty.Register("HorizontalOffset", typeof(int), typeof(DiffControl),
+			new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsRender /*| FrameworkPropertyMetadataOptions.AffectsMeasure*/));
+
+		public int HorizontallOffset
+		{
+			get { return (int)GetValue(HorizontalOffsetProperty); }
+			set { SetValue(HorizontalOffsetProperty, value); }
 		}
 
 
