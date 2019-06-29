8e52507af2023182df2a06518c01b1e4fcec08db
Jonas Hertzman <jonas.hertzman@gmail.com>
2019-06-29 16:05:32 +0200

    New keyboard shortcuts to move no next/previous file in master detail mode

--- FileDiff/MainWindow.xaml.cs
+++ FileDiff/MainWindow.xaml.cs
@@ -1423,6 +1423,26 @@ namespace FileDiff
 			e.CanExecute = ViewModel.FileVissible && lastDiff > ViewModel.CurrentDiff && NoManualEdit;
 		}
 
+		private void CommandNextFile_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+
+		}
+
+		private void CommandNextFile_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = ViewModel.FolderVissible;
+		}
+
+		private void CommandPreviousFile_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+
+		}
+
+		private void CommandPreviousFile_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+
+		}
+
 		private void CommandFind_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
 			SearchPanel.Visibility = Visibility.Visible;
