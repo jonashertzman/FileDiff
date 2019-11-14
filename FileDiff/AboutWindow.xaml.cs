e34e273ba1f10c5a77c094b703442aa59ae5456e
Jonas Hertzman <jonas.hertzman@gmail.com>
2019-11-14 22:22:46 +0100

    Mail link in about window

--- FileDiff/AboutWindow.xaml.cs
+++ FileDiff/AboutWindow.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
 
 namespace FileDiff
 {
-	/// <summary>
-	/// Interaction logic for AboutWindow1.xaml
-	/// </summary>
 	public partial class AboutWindow : Window
 	{
 
@@ -17,6 +15,16 @@ namespace FileDiff
 		private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
 		{
 			Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+			e.Handled = true;
+		}
+
+		private void Feedback_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			MainWindowViewModel viewModel = DataContext as MainWindowViewModel;
+
+			string mailto = Uri.EscapeUriString($"mailto:jonashertzmansoftware@gmail.com?Subject={viewModel.FullApplicationName}&Body=Hello");
+
+			Process.Start(new ProcessStartInfo(mailto));
 			e.Handled = true;
 		}
 
