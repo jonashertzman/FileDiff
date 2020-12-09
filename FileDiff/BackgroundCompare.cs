2a7fffffad1c3b95012904137fe38ac434104450
Jonas Hertzman <jonas.hertzman@gmail.com>
2020-12-09 22:44:35 +0100

    Code cleanup

--- FileDiff/BackgroundCompare.cs
+++ FileDiff/BackgroundCompare.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
-using System.Windows.Documents;
 
 namespace FileDiff
 {
--- FileDiff/OptionsWindow.xaml.cs
+++ FileDiff/OptionsWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
