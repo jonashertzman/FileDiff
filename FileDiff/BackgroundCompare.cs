ccf8904f54afe361ea65c1765cbfb0089e03675b
Jonas Hertzman <jonas.hertzman@gmail.com>
2021-01-03 17:17:52 +0100

    Shift down fix

--- FileDiff/BackgroundCompare.cs
+++ FileDiff/BackgroundCompare.cs
@@ -118,12 +118,12 @@ namespace FileDiff
 				if (rightLines[i].Type == TextState.New)
 				{
 					int diffEnd = i;
-					while (rightLines.Count > diffEnd && rightLines[diffEnd].Type == TextState.New && rightLines[diffEnd].Type == TextState.FullMatch)
+					while (rightLines.Count > diffEnd && rightLines[diffEnd].Type == TextState.New)
 					{
 						diffEnd++;
 					}
 
-					while (rightLines.Count > diffEnd && rightLines[i].Text == leftLines[diffEnd].Text)
+					while (rightLines.Count > diffEnd && rightLines[i].Text == leftLines[diffEnd].Text && rightLines[diffEnd].Type == TextState.FullMatch)
 					{
 						(leftLines[i], leftLines[diffEnd]) = (leftLines[diffEnd], leftLines[i]);
 						(rightLines[i], rightLines[diffEnd]) = (rightLines[diffEnd], rightLines[i]);
