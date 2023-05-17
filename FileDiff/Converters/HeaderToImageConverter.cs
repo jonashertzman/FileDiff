39ee27d99d4634e03d1c678bdf6399995e8f1cc4
Jonas Hertzman <jonas.hertzman@gmail.com>
2023-05-17 23:38:07 +0200

    Code cleanup

---
+++ FileDiff/Converters/HeaderToImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FileDiff;
+
+[ValueConversion(typeof(string), typeof(bool))]
+public class HeaderToImageConverter : IValueConverter
+{
+
+	public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is DriveInfo)
+		{
+			return Application.Current.FindResource("DriveIcon");
+		}
+		else if (value is DirectoryInfo)
+		{
+			return Application.Current.FindResource("FolderIcon");
+		}
+		else
+		{
+			return Application.Current.FindResource("FileIcon");
+		}
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException("Cannot convert back");
+	}
+
+}
---
+++ FileDiff/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FileDiff;
+
+public class InverseBooleanToVisibilityConverter : IValueConverter
+{
+
+	private readonly BooleanToVisibilityConverter converter = new BooleanToVisibilityConverter();
+
+	public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+	{
+		var result = converter.Convert(value, targetType, parameter, culture) as Visibility?;
+		return result == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+	{
+		var result = converter.ConvertBack(value, targetType, parameter, culture) as bool?;
+		return result != true;
+	}
+
+}
