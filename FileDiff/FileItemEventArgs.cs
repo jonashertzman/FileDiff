a0fb63507474041e388f6a647f2382b70ea6a71e
Jonas Hertzman <jonas.hertzman@gmail.com>
2024-10-18 17:31:54 +0200

    Fix TreeControl SelectionChanged event

---
+++ FileDiff/FileItemEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace FileDiff;
+
+public class FileItemEventArgs : EventArgs
+{
+	public FileItem SelectedItem { get; }
+
+	public FileItemEventArgs(FileItem fileItem)
+	{
+		SelectedItem = fileItem;
+	}
+}
