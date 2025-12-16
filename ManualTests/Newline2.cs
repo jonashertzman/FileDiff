f6142f8cabb4e5e0a22b1793487c8ca36b7c1995
Jonas Hertzman <jonas.hertzman@gmail.com>
2025-12-16 17:37:15 +0100

    Add manual test for newline diffs

---
+++ ManualTests/Newline2.cs
@@ -0,0 +1,28 @@
+﻿namespace FileDiff;
+
+public enum TextState
+{
+	FullMatch,
+	PartialMatch,
+	Deleted,
+	New,
+	Filler,
+	Ignored,
+	MovedFrom,
+	MovedTo,
+	MovedFiller,
+}
+
+public enum CompareMode
+{
+	File,
+	Folder,
+}
+
+public enum NewlineMode
+{
+	Windows,
+	Unix,
+	Mac,
+	Mixed,
+}
